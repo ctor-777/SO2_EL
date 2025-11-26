acce3c52030ea8db74f8b9a9388e4bb5dc0e8883
Héctor Rodríguez <ctor777@gmail.com>
2025-11-26 21:12:33 +0100

    milstone 3 and 5 completed.
    same sequences as ANSI

--- include/io.h
+++ include/io.h
@@ -10,9 +10,57 @@
 /** Screen functions **/
 /**********************/
 
+// ----------- seqences ------------
+// change bg color: ESC -> 4 -> color(1-2 chars) -> m
+// change fg color: ESC -> 3 -> color(1-2 chars) -> m
+// change cursor: ESC -> x(1-2 chars) -> ; -> y(1-2 chars) -> H/f
+
+typedef enum {
+	NO_SEQ,
+	INIT_SEQ,
+
+	CHANGE_BG_COLOR_SEL,
+
+	CHANGE_FG_COLOR_SEL,
+
+	CHANGE_POS_X,
+	CHANGE_POS_Y,
+} seq_state;
+
+typedef enum {
+    /* Dark Colors (0x0 - 0x7) */
+    VGA_COLOR_BLACK         = 0x0,
+    VGA_COLOR_BLUE          = 0x1,
+    VGA_COLOR_GREEN         = 0x2,
+    VGA_COLOR_CYAN          = 0x3,
+    VGA_COLOR_RED           = 0x4,
+    VGA_COLOR_MAGENTA       = 0x5,
+    VGA_COLOR_BROWN         = 0x6,
+    VGA_COLOR_LIGHT_GRAY    = 0x7,
+
+    /* Light/Bright Colors (0x8 - 0xF) */
+    VGA_COLOR_DARK_GRAY     = 0x8,
+    VGA_COLOR_LIGHT_BLUE    = 0x9,
+    VGA_COLOR_LIGHT_GREEN   = 0xA,
+    VGA_COLOR_LIGHT_CYAN    = 0xB,
+    VGA_COLOR_LIGHT_RED     = 0xC,
+    VGA_COLOR_LIGHT_MAGENTA = 0xD,
+    VGA_COLOR_YELLOW        = 0xE,
+    VGA_COLOR_WHITE         = 0xF
+} vga_color;
+
+#define is_num(x) ((x >= '0') && (x <= '9'))
+
 Byte inb (unsigned short port);
 void printc(char c);
 void printc_xy(Byte x, Byte y, char c);
 void printk(char *string);
+
+
+
+// temporal
+void change_bg_color(vga_color color);
+
+void change_fg_color(vga_color color);
 
 #endif  /* __IO_H__ */
--- user.c
+++ user.c
@@ -10,5 +10,10 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-  while(1) { }
+	write(1, "\n33m45mthis should be blue\n", 29);
+	write(1, "35m43mthis should be magenta\n", 31);
+	write(1, "3;3Hthis should be somewhere else", 35);
+	  while(1) { 
+
+	}
 }
